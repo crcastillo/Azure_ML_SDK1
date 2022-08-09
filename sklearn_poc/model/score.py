8a1398e64ee51e135cc5645633e45c306a75f4bf
crcastillo <crcastillo84@gmail.com>
2022-08-09 03:52:46 +0000

    add score script

---
+++ sklearn_poc/model/score.py
@@ -0,0 +1,59 @@
+from pyexpat import model
+import joblib
+from azureml.core import Model
+import os
+import pandas as pd
+import numpy as np
+
+from inference_schema.schema_decorators import input_schema, output_schema
+from inference_schema.parameter_types.numpy_parameter_type import NumpyParameterType
+from inference_schema.parameter_types.pandas_parameter_type import PandasParameterType
+from inference_schema.parameter_types.standard_py_parameter_type import StandardPythonParameterType
+    
+data_sample = PandasParameterType(
+    pd.DataFrame(
+        {'WifesAge': pd.Series([0], dtype='int64')
+        , 'WifesEducation': pd.Series(['example_value'], dtype='object')
+        , 'HusbandsEducation': pd.Series(['example_value'], dtype='object')
+        , 'NumberOfChildren': pd.Series([0], dtype='int64')
+        , 'WifesReligion': pd.Series([0], dtype='int64')
+        , 'WifeWorking': pd.Series(['example_value'], dtype='object')
+        , 'HusbandOccupation': pd.Series(['example_value'], dtype='object')
+        , 'StandardOfLivingIndex': pd.Series(['example_value'], dtype='object')
+        , 'MediaExposure': pd.Series(['example_value'], dtype='object')
+        }
+    )
+)
+
+input_sample = StandardPythonParameterType({'data': data_sample})
+result_sample = NumpyParameterType(np.array([0]))
+output_sample = StandardPythonParameterType({'Results':result_sample})
+
+@input_schema('Inputs', input_sample)
+@output_schema(output_sample)
+
+def init():    
+    global preprocess, model
+    # Load preprocessing
+    preprocess_path = Model.get_model_path(model_name='sklearn_preprocessing')
+    print('Preprocessing path is ', preprocess_path)
+    preprocess = joblib.load(preprocess_path)
+
+    # Load model
+    model_path = Model.get_model_path(model_name='sklearn_logistic')
+    print('Model path is ', model_path)
+    model = joblib.load(model_path)
+
+def run(Inputs):
+    try:
+        # Load from json
+        data = Inputs['data']
+        # Run through preprocessing pipeline
+        processed_data = preprocess.transform(data['data'])
+        # Run processed_data through model
+        result = model.predict_proba(processed_data)
+
+        return {'data': result.to_list(), 'message': 'Successfully created predictions'}
+    except Exception as err:
+        error = str(err)
+        return {'data': error, 'message': 'Failed to create predictions'}
---
+++ sklearn_poc/run_score.py
@@ -0,0 +1,63 @@
+#%%
+from azureml.core import (
+    Workspace
+    , Environment
+    )
+from azureml.core.model import (
+    InferenceConfig
+    , Model
+    )
+from azureml.core.webservice import AciWebservice
+
+# Define workspace
+ws = Workspace.from_config()
+
+# Build environment
+env = Environment.from_conda_specification(name='test_env', file_path="conda_dependencies.yml")
+
+# Register the environment
+env.register(workspace=ws)
+
+aci_config = AciWebservice.deploy_configuration(
+    cpu_cores=1
+    , memory_gb=2
+    , tags={
+        'data':'1987 NICP Survey contraception classifier'
+    }
+    , description='Classification of contraception use'
+)
+
+# Create the configuration
+inference_config = InferenceConfig(
+    entry_script='score.py'
+    , source_directory='./model'
+    , environment=env
+)
+
+#%%
+
+# Model deployment config
+service = Model.deploy(
+    workspace=ws
+    , name='nicp-model-log'
+    , models=[
+        Model._get(
+            workspace=ws
+            , name='sklearn_preprocessing'
+        )
+        , Model._get(
+            workspace=ws
+            , name='sklearn_logistic'
+        ) 
+        ]
+    , inference_config=inference_config
+    , deployment_config=aci_config
+    , overwrite = True
+    )
+
+#%%
+
+# Deploy
+service.wait_for_deployment(show_output=True)
+url = service.scoring_uri
+print(url)
